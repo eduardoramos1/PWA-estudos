f7bd226df47b253f3a203f1264daef1ac5f55958
Eduardo Ramos <eduardorxs@gmail.com>
2020-05-20 19:09:46 -0300

    Fetch event e interceptando requisições

--- lifecycle-1/main.js
+++ lifecycle-1/main.js
@@ -1,11 +1,16 @@
 // Progressive Enhancement (SW supported)
 // if ('serviceWorker' in navigator) {
 if (navigator.serviceWorker) {
-	// Register the SW
-	navigator.serviceWorker
-		.register("./sw.js")
-		.then(function (registration) {
-			console.log("SW Registered");
-		})
-		.catch(console.log);
+	navigator.serviceWorker.register("./sw.js").then(function (registration) {
+		console.log("SW registrado");
+	});
 }
+
+// get camera feed
+fetch("camera_feed.html")
+	.then((res) => {
+		return res.text();
+	})
+	.then((html) => {
+		document.getElementById("camera").innerHTML = html;
+	});
--- lifecycle-1/sw.js
+++ lifecycle-1/sw.js
@@ -1,12 +1,31 @@
-
 // Service Worker
+self.addEventListener("fetch", (ev) => {
+	// verifica se a url da requisição termina com "style.css"
+	// if (ev.request.url.endsWith("style.css")) {
+	// 	console.log("Fetch Event for css " + ev.request.url);
+	// 	// Responde a requisição com outro valor
+	// 	ev.respondWith(fetch("./style2.css"));
+	// }
+	// if (ev.request.url.endsWith("/greet")) {
+	// 	// criando um objeto de resposta
+	// 	let headers = new Headers({
+	// 		"Content-Type": "text/html",
+	// 	});
+	// 	let customRes = new Response("<h1>Hello World!</h1>", {
+	// 		headers,
+	// 	});
+	// 	ev.respondWith(customRes);
+	// }
 
-self.addEventListener('install', (e) => {
+	if (ev.request.url.endsWith("/camera_feed.html")) {
+		// se a requisição for feita com o final 'camera_feed.html, tenta fazer
+		//  a requisição normalmente
+		ev.respondWith(
+			fetch(ev.request).then((res) => {
+				if (res.ok) return res;
 
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-
-  console.log('SW3 Activated');
+				return new Response("Camera feed não disponível");
+			})
+		);
+	}
 });
